7e77d899090b2fd208316f25c059ba72d320a828
ajaysagitx <156707389+ajaysagitx@users.noreply.github.com>
2024-01-23 21:51:13 -0600

    Add files via upload

---
+++ Week 2 Practice.py
@@ -0,0 +1,273 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num=5
+
+
+# In[3]:
+
+
+type(num)
+
+
+# In[4]:
+
+
+num=23475891
+
+
+# In[5]:
+
+
+type(num)
+
+
+# In[6]:
+
+
+num=5.4
+
+
+# In[7]:
+
+
+type(num)
+
+
+# In[8]:
+
+
+num=2+5j
+
+
+# In[9]:
+
+
+type(num)
+
+
+# In[10]:
+
+
+num.real
+
+
+# In[11]:
+
+
+num.imag
+
+
+# In[12]:
+
+
+a=5
+print("Type of a: ", type(a))
+
+b=5.0
+print("\nType of b: ", type(b))
+
+c=2+4j
+print("\nType of c: ", type(c))
+
+
+# In[13]:
+
+
+a=5
+print("Type of a: ", type(a))
+
+b=5.0
+print("Type of b: ", type(b))
+
+c=2+4j
+print("Type of c: ", type(c))
+
+
+# In[14]:
+
+
+d=complex(4,-1)
+
+
+# In[15]:
+
+
+print(d)
+
+
+# In[16]:
+
+
+complex(2,4)-complex(1,6)
+
+
+# In[17]:
+
+
+numbercollegegraduate=7500
+NumberCollegeGraduate=7500
+
+
+# In[18]:
+
+
+print(numbercollegegraduate,NumberCollegeGraduate)
+
+
+# In[19]:
+
+
+def hello():
+    print("Hello there")
+    
+    hello()
+
+
+# In[20]:
+
+
+#Simple Python
+def hello():
+    print("Hello there")
+    
+#First Time    
+hello()
+
+
+# In[ ]:
+
+
+h=1
+
+
+# In[ ]:
+
+
+ha=2
+
+
+# In[ ]:
+
+
+hah=3
+
+
+# In[21]:
+
+
+print(h,ha,hah)
+
+
+# In[22]:
+
+
+max(15,27)
+
+
+# In[23]:
+
+
+#create string
+
+name = 'Python'
+print(name)
+
+message='I love Python'
+print(message)
+
+
+# In[24]:
+
+
+name = 'Python'
+print(name)
+
+message='I love Python'
+print(message)
+
+
+# In[25]:
+
+
+name = 'Python'
+print(name)
+
+message='I hate Python'
+print(message)
+
+
+# In[26]:
+
+
+a="hello world"
+print(a[1])
+
+
+# In[27]:
+
+
+a="hello world"
+print(a[0])
+
+
+# In[28]:
+
+
+a="hello world"
+print(a[8])
+
+
+# In[31]:
+
+
+for x in "Ajay Sagi":
+print(x)
+
+
+# In[35]:
+
+
+for x in "Ajay Sagi":
+    print(x)
+
+
+# In[34]:
+
+
+a="hello world"
+print(len(a))
+
+
+# In[37]:
+
+
+str1="Hello"
+str2="Ajay"
+str3="Hello"
+
+print(str1==str2)
+
+print(str1==str3)
+
+print(str2==str3)
+
+
+# In[43]:
+
+
+a='Hello,michael'
+print(a[-3:-1])
+print(a[3:7])
+print(a[-1:])
+print(a[2:])
+
+
+# In[ ]:
+
+
+
+
